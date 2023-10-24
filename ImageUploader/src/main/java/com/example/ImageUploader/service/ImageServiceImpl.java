b9f26bac69dc356e648b5b62f57b5dc0bdbc0a4a
LCBA21 <lesiban56@gmail.com>
2023-10-24 11:20:43 +0200

    Upadated ImageServiceImpl with overrided methods

--- ImageUploader/src/main/java/com/example/ImageUploader/service/ImageServiceImpl.java
+++ ImageUploader/src/main/java/com/example/ImageUploader/service/ImageServiceImpl.java
@@ -9,4 +9,19 @@ import java.util.List;
 @Service
 public class ImageServiceImpl implements ImageService {
 
+    @Autowired
+    private ImageRepository imageRepository;
+
+    @Override
+    public Image create(Image image) {
+        return imageRepository.save(image);
+    }
+    @Override
+    public List<Image> viewAll() {
+        return (List<Image>) imageRepository.findAll();
+    }
+    @Override
+    public Image viewById(long id) {
+        return imageRepository.findById(id).get();
+    }
 }
